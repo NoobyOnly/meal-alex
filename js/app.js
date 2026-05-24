c00994f81c4a259c455ddead19343832dce0d058
NoobyOnly <48660672+NoobyOnly@users.noreply.github.com>
2026-05-24 22:17:00 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+function showTab(tabId) {
+
+  document
+    .querySelectorAll('.tabContent')
+    .forEach(tab => {
+      tab.classList.add('hidden');
+    });
+
+  document
+    .getElementById(tabId)
+    .classList.remove('hidden');
+}
+
+
+function renderApp() {
+
+  renderPlanner();
+
+  renderMealsList();
+
+  updateStats();
+
+  updateTotals();
+}
+
+
+renderApp();
 
