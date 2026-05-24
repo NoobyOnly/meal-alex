62dfe0f73136046b926b5634efb4821b995c5219
NoobyOnly <48660672+NoobyOnly@users.noreply.github.com>
2026-05-24 23:56:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,7 @@
-const CACHE = "meal-planner-v1";
-const ASSETS = [
-  "/meal-alex/",
-  "/meal-alex/index.html",
-  "/meal-alex/icon.svg",
-  "/meal-alex/manifest.json"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  // Pusta sve mreze normalno ka Firebase-u
 });
